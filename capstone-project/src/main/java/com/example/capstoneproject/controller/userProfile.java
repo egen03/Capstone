6a952f4a679e9a1386238498fed5c1a546deb1e1
student_ga111 <egen03@gmail.com>
2020-09-01 18:41:59 -0400

    created get request for api endpoint

--- capstone-project/src/main/java/com/example/capstoneproject/controller/userProfile.java
+++ capstone-project/src/main/java/com/example/capstoneproject/controller/userProfile.java
@@ -1,7 +1,14 @@
 package com.example.capstoneproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Rest Controller endpoints for api
 @RestController("/userprofile")
 public class userProfile {
+//    Get request for api
+    @GetMapping
+    public String listOfTops() {
+        return "Shirts";
+    }
 }
