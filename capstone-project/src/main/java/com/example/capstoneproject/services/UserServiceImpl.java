d27ec8b87cf4d0e0eec30a63f027439d68d49dca
student_ga111 <egen03@gmail.com>
2020-09-02 00:29:05 -0400

    shifted info from controller to implement list of users

--- capstone-project/src/main/java/com/example/capstoneproject/services/UserServiceImpl.java
+++ capstone-project/src/main/java/com/example/capstoneproject/services/UserServiceImpl.java
@@ -2,17 +2,23 @@ package com.example.capstoneproject.services;
 
 import com.example.capstoneproject.models.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService{
     @Override
     public User getUser() {
+//        controller info:
         User user = new User("jewel", "password5");
         return user;
     }
 
     @Override
     public List<User> getUsers() {
-        return null;
+        List<User> users = new ArrayList<User>();
+        users.add(new User("Ayana", "password2"));
+        users.add(new User("Jeffrey", "password3"));
+        users.add(new User("Lexy", "password4"));
+        return users;
     }
 }
