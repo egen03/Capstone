55a13a3e0135d712b5ee058c7e481405c411dcb2
student_ga111 <egen03@gmail.com>
2020-09-02 00:10:15 -0400

    created method to get a user

--- capstone-project/src/main/java/com/example/capstoneproject/services/UserService.java
+++ capstone-project/src/main/java/com/example/capstoneproject/services/UserService.java
@@ -1,4 +1,6 @@
 package com.example.capstoneproject.services;
 
+// this interface will have 2 methods return a user and a list of users
 public interface UserService {
+    public User getUser();
 }
