f151fb825287011c5265b7bc142dee1199f5cf58
student_ga111 <egen03@gmail.com>
2020-09-02 00:11:06 -0400

    imported User class

--- capstone-project/src/main/java/com/example/capstoneproject/services/UserService.java
+++ capstone-project/src/main/java/com/example/capstoneproject/services/UserService.java
@@ -1,5 +1,7 @@
 package com.example.capstoneproject.services;
 
+import com.example.capstoneproject.models.User;
+
 // this interface will have 2 methods return a user and a list of users
 public interface UserService {
     public User getUser();
