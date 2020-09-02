701a4319d1d8078cc8b9dc18d15f720706cf52de
student_ga111 <egen03@gmail.com>
2020-09-01 23:18:04 -0400

    imported User file to userController

--- capstone-project/src/main/java/com/example/capstoneproject/controller/UserController.java
+++ capstone-project/src/main/java/com/example/capstoneproject/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.capstoneproject.controller;
 
+import com.example.capstoneproject.models.User;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
