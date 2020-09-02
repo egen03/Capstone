5f6c0f32df33e66b5dc5c9bcbbfb575980bde903
student_ga111 <egen03@gmail.com>
2020-09-01 20:26:24 -0400

    created getters setters for username password

--- capstone-project/src/main/java/com/example/capstoneproject/models/User.java
+++ capstone-project/src/main/java/com/example/capstoneproject/models/User.java
@@ -1,4 +1,27 @@
 package com.example.capstoneproject.models;
 
 public class User {
+//    2 fields of user - username &password
+    private String username;
+    private String password;
+
+//    getters and setters for username
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+//    getters and setters for password
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
