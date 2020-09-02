b0c592d0883a33269c9bd62829099fe06ef9f43d
student_ga111 <egen03@gmail.com>
2020-09-01 23:16:31 -0400

    created userController using restcontroller

--- capstone-project/src/main/java/com/example/capstoneproject/controller/UserController.java
+++ capstone-project/src/main/java/com/example/capstoneproject/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.example.capstoneproject.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/user")
+
 public class UserController {
+//    method goes here
+
+//    endpoint "/user"
+//    takes GET request
+//    returns User object
+    public String userOne() {
+        User userOne = new User("erica", "password")
+        return "username";
+    }
 }
