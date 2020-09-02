2c3dcb0815e0c06753364ff31ece0d4946479080
student_ga111 <egen03@gmail.com>
2020-09-01 23:30:45 -0400

    created api for single user

--- capstone-project/src/main/java/com/example/capstoneproject/controller/UserController.java
+++ capstone-project/src/main/java/com/example/capstoneproject/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.capstoneproject.controller;
 
 import com.example.capstoneproject.models.User;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,9 @@ public class UserController {
 //    endpoint "/user"
 //    takes GET request
 //    returns User object
-    public String userOne() {
-        User userOne = new User("erica", "password")
-        return "username";
+    @GetMapping
+    public User userOne() {
+//        User userOne = new User("erica", "password");
+        return new User("erica", "password");
     }
 }
