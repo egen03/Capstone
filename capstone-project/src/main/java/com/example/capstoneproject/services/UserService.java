013b2de38971200db0abdd662303ea9737e0975c
student_ga111 <egen03@gmail.com>
2020-09-02 00:13:41 -0400

    created method to get list of users using List function

--- capstone-project/src/main/java/com/example/capstoneproject/services/UserService.java
+++ capstone-project/src/main/java/com/example/capstoneproject/services/UserService.java
@@ -2,7 +2,10 @@ package com.example.capstoneproject.services;
 
 import com.example.capstoneproject.models.User;
 
+import java.util.List;
+
 // this interface will have 2 methods return a user and a list of users
 public interface UserService {
     public User getUser();
+    public List<User> getUsers();
 }
