307302374d5c7c35d1d4841dcd60aafedb8d540b
student_ga111 <egen03@gmail.com>
2020-09-02 00:18:54 -0400

    implemented UserService

--- capstone-project/src/main/java/com/example/capstoneproject/services/UserServiceImpl.java
+++ capstone-project/src/main/java/com/example/capstoneproject/services/UserServiceImpl.java
@@ -1,4 +1,17 @@
 package com.example.capstoneproject.services;
 
-public class UserServiceImpl {
+import com.example.capstoneproject.models.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService{
+    @Override
+    public User getUser() {
+        return null;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
 }
