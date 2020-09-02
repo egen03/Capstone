35685f4e0983bcc701fce60882d355001d2b9369
student_ga111 <egen03@gmail.com>
2020-09-02 00:53:13 -0400

    used autowire annotations to get users

--- capstone-project/src/main/java/com/example/capstoneproject/controller/UserController.java
+++ capstone-project/src/main/java/com/example/capstoneproject/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.capstoneproject.models.User;
 
 import com.example.capstoneproject.services.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,27 +14,18 @@ import java.util.*;
 @RequestMapping ("/user")
 
 public class UserController {
-//    method goes here
+    @Autowired
+    UserService userService;
 
-//    endpoint "/user"
-//    takes GET request
-//    returns User object
     @GetMapping
     public User userOne() {
-//        User userOne = new User("erica", "password");
-//        return new User("erica", "password");
+        return userService.getUser();
     }
 
 //    create endpoint for list of all users
     @GetMapping("/listall")
 //    java function List<> used to list all users
     public List<User> listAll() {
-//     list of users
-//        List<User> users = new ArrayList<User>();
-//     create user objects
-//        users.add(new User("Ayana", "password2"));
-//        users.add(new User("Jeffrey", "password3"));
-//        users.add(new User("Lexy", "password4"));
-//        return users;
+        return userService.getUsers();
     }
 }
