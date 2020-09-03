8247ef78e7b4bc4b0e1d8b06428f21c50c9eae22
student_ga111 <egen03@gmail.com>
2020-09-03 01:01:13 -0400

    entity and table annotations columns for database

--- capstone-project/src/main/java/com/example/capstoneproject/models/UserP.java
+++ capstone-project/src/main/java/com/example/capstoneproject/models/UserP.java
@@ -2,5 +2,21 @@ package com.example.capstoneproject.models;
 
 import javax.persistence.*;
 
+@Entity
+@Table(name = "Inventory")
 public class UserP {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    public String merch;
+
+    @Column
+    public String Tops;
+
+    @Column
+    public String Pants;
+
+    @Column
+    public String Dresses;
 }
