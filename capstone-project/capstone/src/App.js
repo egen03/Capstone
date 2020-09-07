a04b13a77e0c6f6da6f000619bdc4eb95b63c620
student_ga111 <egen03@gmail.com>
2020-09-07 00:07:38 -0400

    created login form

--- capstone-project/capstone/src/App.js
+++ capstone-project/capstone/src/App.js
@@ -1,10 +1,12 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
 function App() {
-//    async function provides connection to React app
+    const [input, setInput] = useState({username: "", password: ""});
+
+//    async function provides connection to React app. Make a call to grab all users
     async function grabAllUsers() {
         try {
             const res = await axios.get('http://localhost:8181/user');
@@ -16,12 +18,49 @@ function App() {
 
 //    useEffect due to use of functional components
     useEffect(() => {
-    })
+        grabAllUsers();
+    }, [])
+
+    function handleChange(e) {
+        const { value, id } = e.target;
+        console.log(value, id);
+        setInput({ ...input, [id]: value });
+    }
+
+// Submit button functionality
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            const res = await axios.post('http://localhost:8181/user', input);
+            console.log(res.data);
+        } catch(e) {
+            console.error(e, e.message);
+        }
+    }
 
   return (
     <div className="App">
       <header className="App-header">
 
+        <div className="Form">
+        <form onChange={ (e) => handleChange(e) } onSubmit={ (e) => handleSubmit(e)} >
+        <label>
+            username:
+            <input type="text" />
+        </label>
+
+         <label>
+             password:
+             <input type="password" id="password" />
+         </label>
+
+
+
+              <input type="submit" />
+
+
+        </form>
+        </div>
       </header>
     </div>
   );
--- capstone-project/src/main/java/com/example/capstoneproject/controller/UserController.java
+++ capstone-project/src/main/java/com/example/capstoneproject/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping ("/user")
 
 public class UserController {
